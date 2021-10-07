cbac75dca23215c8443b10cfb1b14281ef940ed3
Rosariobacchin <69010565+Rosariobacchin@users.noreply.github.com>
2021-10-07 12:05:38 -0300

    Funciones __init__ y __str__registro terminado
    despues corregir __str__ para el tamaño de los libros

--- Registro.py
+++ Registro.py
@@ -1,8 +1,15 @@
-# registro
-
 class Libro:
-    def __init__(self):
-        pass
+    def __init__(self, titulo, cant_rev, año, cod_idioma, rating, isbn):
+        self.titulo = titulo
+        self.cant_rev = cant_rev
+        self.año = año
+        self.cod_idoma = cod_idioma
+        self.rating = rating
+        self.isbn = isbn
 
     def __str__(self):
-        pass
+        s = '|Título:{:<20} |Cantidad de revisiones: {:>4} |Año {:>7} |Codigo del idioma: {:>4} |Rating: {:>4} |ISBN: {:>10} '
+        s = s.format(self.titulo, self.cant_rev, self.año, self.cod_idoma, self.rating, self.isbn)
+        return s
+
+
